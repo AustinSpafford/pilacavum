c24c0e2d9489d409ce4a51e51eb2da9f877de7ab
Austin Spafford <austin.spafford@gmail.com>
2016-05-11 22:16:20 -0700

    Allowing the balls to be gripped and released via the trigger

---
+++ Assets/Pilacavum/Scripts/GripperController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GripperController : MonoBehaviour
+{
+	public void Awake()
+	{
+		trackedController = GetComponentInParent<SteamVR_TrackedController>();
+		velociGripper = GetComponentInParent<VelociGripper>();
+
+		trackedController.TriggerClicked += OnTriggerClicked;
+		trackedController.TriggerUnclicked += OnTriggerUnclicked;
+	}
+
+	private void OnTriggerClicked(
+		object sender,
+		ClickedEventArgs eventArgs)
+	{
+		GameObject nearestGrabbableObject = null;
+		float nearestGrabbableObjectDistanceSquared = float.MaxValue;
+
+		// This is super-janky!
+		// ...
+		// *shrug*
+		foreach (GameObject candidate in GameObject.FindGameObjectsWithTag("Grabbable"))
+		{
+			float candidateDistanceSquared =
+				(transform.position - candidate.transform.position).sqrMagnitude;
+
+			if (candidateDistanceSquared < nearestGrabbableObjectDistanceSquared)
+			{
+				nearestGrabbableObject = candidate;
+				nearestGrabbableObjectDistanceSquared = candidateDistanceSquared;
+			}
+		}
+
+		if (nearestGrabbableObject != null)
+		{
+			velociGripper.GripObject(nearestGrabbableObject);
+		}
+	}
+
+	private void OnTriggerUnclicked(
+		object sender,
+		ClickedEventArgs eventArgs)
+	{
+		velociGripper.ReleaseObject();
+	}
+
+	private SteamVR_TrackedController trackedController = null;
+	private VelociGripper velociGripper = null;
+}
